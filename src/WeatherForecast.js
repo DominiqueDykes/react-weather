e598097a1ed370302bc6e72f2c527ec29b38836b
DominiqueDykes <nique.dykes@gmail.com>
2023-05-31 12:36:13 -0400

    changed degree to fahrenheit

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,15 +16,6 @@ export default function WeatherForecast(props) {
     setLoaded(true);
   }
 
-  function load() {
-    let apiKey = "c7168fc898beb3d71d7ec7bf066fa119";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
-  }
-
   if (loaded) {
     return (
       <div className="WeatherForecast">
@@ -44,8 +35,11 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    load();
+    let apiKey = "16521191ba0767009779ecc5b524d3b19";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
 
-    return null;
+    axios.get(apiUrl).then(handleResponse);
   }
 }
