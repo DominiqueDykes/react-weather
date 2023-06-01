03215b2350d4daa47807a8a043de8684d079fe04
DominiqueDykes <nique.dykes@gmail.com>
2023-06-01 11:40:27 -0400

    updated weather forecast and api

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,10 +17,10 @@ export default function WeatherForecast(props) {
   }
 
   function load() {
-    let apiKey = "c7168fc898beb3d71d7ec7bf066fa119";
+    let apiKey = "93d43dfe3b4a950e5b187e5dc313705e";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
 
     axios.get(apiUrl).then(handleResponse);
 
